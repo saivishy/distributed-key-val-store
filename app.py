c845cd85ee5f95f36ed3b37d9010f67692057123
Arnav Sinha <arnasin@buffalo.edu>
2022-05-05 21:58:48 -0400

    updated store log definition and call

--- app.py
+++ app.py
@@ -185,7 +185,7 @@ def heartBeatSend(skt, hb_interval = 10):
                                         , success = None
                                         , entry = entry)
                         skt.sendto(msg, (f"Node{node}", 5555))
-                        print(f"HEARTBEAT TO Node{node} SENT!")
+                        print(f"HEARTBEAT TO Node{node} SENT! msg : {msg}")
                     except:
                         print(f"Node{node} not reachable - {traceback.print_exc()}")
                         active_node_count =  active_node_count -1
@@ -381,24 +381,38 @@ def store_log(log_key, value):
     log_file_name = os.environ.get("NODEID") + "_logs.json"
     try: 
         json_obj = json.load(open(log_file_name, "r"))
-        log_entry = {
-            "term" : int(os.environ.get("current_term")),
-            "key" :value["key"],
-            "value": value["value"]
-        }
+        if value["sender_name"] == "Controller":
+            log_entry = {
+                "term" : int(os.environ.get("current_term")),
+                "key" :value["key"],
+                "value": value["value"]
+            }
+        else:
+            log_entry = {
+                "term" : value["entry"]["term"],
+                "key" :value["entry"]["key"],
+                "value": value["entry"]["value"]
+            }
         json_obj[log_key] = log_entry
         with open(log_file_name, 'w') as f:
             json.dump(json_obj, f, ensure_ascii=False, indent=4)
-        
-        
+
+
     except FileNotFoundError:
         # json_obj = json.load(open(log_file_name, "r"))
         json_obj = {}
-        log_entry = {
-            "term" : int(os.environ.get("current_term")),
-            "key" :value["key"],
-            "value": value["value"]
-        }
+        if value["sender_name"] == "Controller":
+            log_entry = {
+                "term" : int(os.environ.get("current_term")),
+                "key" :value["key"],
+                "value": value["value"]
+            }
+        else:
+            log_entry = {
+                "term" : value["entry"]["term"],
+                "key" :value["entry"]["key"],
+                "value": value["entry"]["value"]
+            }
         json_obj[log_key] = log_entry
         with open(log_file_name, 'w') as f:
             json.dump(json_obj, f, ensure_ascii=False, indent=4)
@@ -406,6 +420,7 @@ def store_log(log_key, value):
     # Update next_log_index
     environ_vars = readJSONInfo(os.environ.get("NODEID") + "_environ_vars.json")
     environ_vars["next_log_index"] = environ_vars["next_log_index"] + 1
+    environ_vars["last_applied_index"] = environ_vars["last_applied_index"] + 1
     writeJSONInfo(os.environ.get("NODEID") + "_environ_vars.json" ,environ_vars)
 
     # os.environ["next_log_index"] = str(int(os.environ.get("next_log_index"))+1)
@@ -566,7 +581,8 @@ def normalRecv(skt): # Common Recv
 
                 # Normal heart beat (leader log to write exists) with entry to be appended to the Followers' log
                 else:
-                    store_log(int(decoded_msg["prevLogIndex"]) + 1 , {"key": None, "value": None})
+                    print(f"message to be stored is {decoded_msg}")
+                    store_log(int(decoded_msg["prevLogIndex"]) + 1 , decoded_msg)
                     heartBeatReplySend(skt=pulse_sending_socket, 
                                         success = True,
                                         prevLogIndex = decoded_msg["prevLogIndex"], 
@@ -590,7 +606,7 @@ def normalRecv(skt): # Common Recv
             
             
             else:
-                if decoded_msg["key"] == "NULL":
+                if decoded_msg["entry"]["key"] == "NULL":
                     print(f'log is consistent ʖ ; for {decoded_msg["sender_name"]}')
                 else:
                     print(f'log accepted ( ͡° ͜ʖ ͡°) ; increasing nextIndex for {decoded_msg["sender_name"]}')
@@ -692,7 +708,8 @@ def normalRecv(skt): # Common Recv
             send_all_info(pulse_sending_socket)
         
         if decoded_msg["request"] == "STORE" and decoded_msg["sender_name"] == "Controller" and os.environ.get("STATE")=="leader":
-            store_log(int(os.environ.get("next_log_index")), decoded_msg)
+            environ_vars = readJSONInfo(os.environ.get("NODEID") + "_environ_vars.json")
+            store_log(int(environ_vars["next_log_index"]), decoded_msg)
             store_ack(pulse_sending_socket)         
         
         if decoded_msg["request"] == "STORE" and decoded_msg["sender_name"] == "Controller" and os.environ.get("STATE")!="leader":
@@ -717,14 +734,15 @@ if __name__ == "__main__":
         json_obj = json.load(open(log_file_name, "r"))
         ## if empty log then give one dummy value
         if json_obj == {}:
-            json_obj = {'0':{"term" :'0',"key" :"dummy","value": "dummy"}}
-            writeJSONInfo(log_file_name,json_obj)    
+            json_obj = {'0':{"term" :1,"key" :"dummy","value": "dummy"}}
+            writeJSONInfo(log_file_name,json_obj)
+            os.environ.get("next_log_index") == "1"    
     
     except FileNotFoundError:
         # json_obj = json.load(open(log_file_name, "r"))
-        json_obj = {'0':{"term" :'0',"key" :"dummy","value": "dummy"}}
+        json_obj = {'0':{"term" :1,"key" :"dummy","value": "dummy"}}
         writeJSONInfo(log_file_name,json_obj)
-
+        
     # Init node_id_environ_vars.json  
     log_file_name = os.environ.get("NODEID") + "_environ_vars.json"  
     try: 
